c475a5e9b5bcf2b839085bc87cd86a5bec4598ec
designpage <designpage@gmail.com>
2022-12-03 01:13:39 +0100

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import { AuthContextProvider } from "../utils/context/AuthContext";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AuthContextProvider>
+      <Component {...pageProps} />
+    </AuthContextProvider>
+  );
 }
 
 export default MyApp
